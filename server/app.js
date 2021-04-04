4d99327d3aaa5b1622805259920edae18d7e2f0b
abc <omkargodham321@gmail.com>
2021-04-04 20:45:12 +0530

    deployed

--- server/app.js
+++ server/app.js
@@ -3,17 +3,21 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const postRoutes = require('./routes/post')
+require("dotenv").config();
+
 const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 
-mongoose.connect("mongodb://localhost:27017/memories",{ useNewUrlParser: true, useUnifiedTopology: true ,useFindAndModify : false})
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true ,useFindAndModify : false}).then(
+    console.log('Connected to DB')
+).catch(err => console.log(err));
 
 app.use('/posts',postRoutes);
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.listen(port , () => {
 console.log(`Running on port ${port}`);
 });
